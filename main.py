7afa052f8cecf9c8362ad8ef16289b44f7826232
bnonni <bryan@nonni.io>
2023-04-16 09:57:40 -0400

    working code example

--- main.py
+++ main.py
@@ -1,37 +1,139 @@
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+import logging
+import datetime
+import time
+from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode, MessageEntity
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 import openai
 from env import TELEGRAM_BOT_TOKEN, OPENAI_API_KEY
 
-# Set up the Telegram bot using your API token
-bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+logger = logging.getLogger(__name__)
 
-# Set up the ChatGPT API using your API key
-openai.api_key = OPENAI_API_KEY
+# Store bot screaming status
+screaming = False
 
-# Define a function to generate a response using the ChatGPT API
-def generate_response(text):
-    prompt = "Conversation with user:\n" + text + "\nAI:"
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
+# Pre-assign menu text
+FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
+SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
+
+# Pre-assign button text
+NEXT_BUTTON = "Next"
+BACK_BUTTON = "Back"
+TUTORIAL_BUTTON = "Tutorial"
+
+# Build keyboards
+FIRST_MENU_MARKUP = InlineKeyboardMarkup([[
+    InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)
+]])
+SECOND_MENU_MARKUP = InlineKeyboardMarkup([
+    [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
+    [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")]
+])
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """
+    This function would be added to the dispatcher as a handler for messages coming from the Bot API
+    """
+
+    # Print to console
+    print(f'{update.message.from_user.first_name} wrote {update.message.text}')
+
+    if screaming and update.message.text:
+        context.bot.send_message(
+            update.message.chat_id,
+            update.message.text.upper(),
+            # To preserve the markdown, we attach entities (bold, italic...)
+            entities=update.message.entities
+        )
+    else:
+        # This is equivalent to forwarding, without the sender's name
+        update.message.copy(update.message.chat_id)
+
+
+def scream(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles the /scream command
+    """
+
+    global screaming
+    screaming = True
+
+
+def whisper(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles /whisper command
+    """
+
+    global screaming
+    screaming = False
+
+
+def menu(update: Update, context: CallbackContext) -> None:
+    """
+    This handler sends a menu with the inline buttons we pre-assigned above
+    """
+
+    context.bot.send_message(
+        update.message.from_user.id,
+        FIRST_MENU,
+        parse_mode=ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
     )
-    return response.choices[0].text
 
-# Define a function to handle incoming messages
-def message_handler(update, context):
-    text = update.message.text
-    response = generate_response(text)
-    bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-# Set up the Telegram bot to listen for incoming messages
-updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), message_handler))
+def button_tap(update: Update, context: CallbackContext) -> None:
+    """
+    This handler processes the inline buttons on the menu
+    """
 
-# Start the bot
-updater.start_polling()
+    data = update.callback_query.data
+    text = ''
+    markup = None
+
+    if data == NEXT_BUTTON:
+        text = SECOND_MENU
+        markup = SECOND_MENU_MARKUP
+    elif data == BACK_BUTTON:
+        text = FIRST_MENU
+        markup = FIRST_MENU_MARKUP
+
+    # Close the query to end the client-side loading animation
+    update.callback_query.answer()
+
+    # Update message content with corresponding menu section
+    update.callback_query.message.edit_text(
+        text,
+        ParseMode.HTML,
+        reply_markup=markup
+    )
+
+
+def main() -> None:
+    print('Main!')
+    updater = Updater(TELEGRAM_BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    # Then, we register each handler and the conditions the update must meet to trigger it
+    dispatcher = updater.dispatcher
+
+    # Register commands
+    dispatcher.add_handler(CommandHandler("scream", scream))
+    dispatcher.add_handler(CommandHandler("whisper", whisper))
+    dispatcher.add_handler(CommandHandler("menu", menu))
+
+    # Register handler for inline buttons
+    dispatcher.add_handler(CallbackQueryHandler(button_tap))
+
+    # Echo any message that is not a command
+    dispatcher.add_handler(MessageHandler(~Filters.command, echo))
+
+    print('Polling!')
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
