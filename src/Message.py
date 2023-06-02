43630bfabc4ff23e3f39ffa86e01f2c6345f539d
bnonni <bryan@nonni.io>
2023-06-02 11:28:36 -0400

    updates

---
+++ src/Message.py
@@ -0,0 +1,75 @@
+Message(channel_chat_created=False, chat=Chat(id=-1001980721880, title='ATL BitLab Bot Testing', type=<ChatType.SUPERGROUP>), date=datetime.datetime(2023, 6, 2, 13, 39, 42, tzinfo=<UTC>), delete_chat_photo=False, from_user=User(first_name='bryan', id=1711738045, is_bot=False, language_code='en', username='nonni_io'), group_chat_created=False, message_id=167, supergroup_chat_created=False, text='Testing 123')
+
+Message(channel_chat_created=False, chat=Chat(id=-1001980721880, title='ATL BitLab Bot Testing', type=<ChatType.SUPERGROUP>), date=datetime.datetime(2023, 6, 2, 13, 40, 36, tzinfo=<UTC>), delete_chat_photo=False, from_user=User(first_name='bryan', id=1711738045, is_bot=False, language_code='en', username='nonni_io'), group_chat_created=False, message_id=169, message_thread_id=166, reply_to_message=Message(channel_chat_created=False, chat=Chat(id=-1001980721880, title='ATL BitLab Bot Testing', type=<ChatType.SUPERGROUP>), date=datetime.datetime(2023, 6, 1, 19, 29, 2, tzinfo=<UTC>), delete_chat_photo=False, from_user=User(first_name='bryan', id=1711738045, is_bot=False, language_code='en', username='nonni_io'), group_chat_created=False, message_id=166, supergroup_chat_created=False, text='Hello from EDT'), supergroup_chat_created=False, text='Testing non-image reply')
+
+Message(
+    channel_chat_created=False,
+    chat=Chat(
+        id=-1001980721880,
+        title='ATL BitLab Bot Testing',
+        type=<ChatType.SUPERGROUP>
+    ),
+    date=datetime.datetime(2023, 6, 2, 13, 40, 7, tzinfo=<UTC>),
+    delete_chat_photo=False,
+    from_user=User(
+        first_name='bryan',
+        id=1711738045,
+        is_bot=False,
+        language_code='en',
+        username='nonni_io'
+    ),
+    group_chat_created=False,
+    message_id=168,
+    message_thread_id=165,
+    reply_to_message=Message(
+            caption='Teswting',
+            channel_chat_created=False,
+            chat=Chat(id=-1001980721880,
+            title='ATL BitLab Bot Testing',
+            type=<ChatType.SUPERGROUP>),
+            date=datetime.datetime(2023, 6, 1, 19, 25, 19, tzinfo=<UTC>),
+            delete_chat_photo=False,
+            from_user=User(
+                first_name='bryan',
+                id=1711738045,
+                is_bot=False,
+                language_code='en',
+                username='nonni_io'
+            ),
+            group_chat_created=False,
+            message_id=165,
+            photo=(
+                PhotoSize(
+                    file_id='AgACAgEAAx0Cdg9q2AADpWR58TfpzuGoT2pDcVOQzPNSCc6OAAIDqzEbz8rARyMKt5IQjFnmAQADAgADcwADLwQ',
+                    file_size=1034,
+                    file_unique_id='AQADA6sxG8_KwEd4',
+                    height=51,
+                    width=90
+                ),
+                PhotoSize(
+                    file_id='AgACAgEAAx0Cdg9q2AADpWR58TfpzuGoT2pDcVOQzPNSCc6OAAIDqzEbz8rARyMKt5IQjFnmAQADAgADbQADLwQ',
+                    file_size=12014,
+                    file_unique_id='AQADA6sxG8_KwEdy',
+                    height=181,
+                    width=320
+                ),
+                PhotoSize(
+                    file_id='AgACAgEAAx0Cdg9q2AADpWR58TfpzuGoT2pDcVOQzPNSCc6OAAIDqzEbz8rARyMKt5IQjFnmAQADAgADeAADLwQ',
+                    file_size=43142,
+                    file_unique_id='AQADA6sxG8_KwEd9',
+                    height=452,
+                    width=800
+                ),
+                PhotoSize(
+                    file_id='AgACAgEAAx0Cdg9q2AADpWR58TfpzuGoT2pDcVOQzPNSCc6OAAIDqzEbz8rARyMKt5IQjFnmAQADAgADeQADLwQ',
+                    file_size=58548,
+                    file_unique_id='AQADA6sxG8_KwEd-',
+                    height=723,
+                    width=1280
+                )
+            ),
+            supergroup_chat_created=False
+        ),
+        supergroup_chat_created=False,
+        text='Testing Reply'
+    )
