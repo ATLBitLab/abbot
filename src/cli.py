e211f58c25461c2a87e04ee24dbfb1fa6e2ce316
bnonni <bryan@nonni.io>
2023-07-22 12:24:17 -0400

    adding nostr tooling

--- src/cli.py
+++ src/cli.py
@@ -1,21 +1,22 @@
 from sys import argv
-from atl_bitlab_bot import clean_jsonl_data, summarize_messages, bot_main
+from atl_bitlab_bot import clean_jsonl_data, summarize_messages
 from lib.nostr.nostr import nostr_main
 ARGS = argv[1:]
 CLEAN = "-c" in ARGS or "--clean" in ARGS
 SUMMARY = "-s" in ARGS or "--summary" in ARGS
 CLEAN_SUMMARY = CLEAN and SUMMARY
 
-if CLEAN:
-    clean_jsonl_data()
+# if CLEAN:
+#     clean_jsonl_data()
 
-elif SUMMARY:
-    summarize_messages()
+# elif SUMMARY:
+#     summarize_messages()
 
-elif CLEAN_SUMMARY:
-    clean_jsonl_data()
-    summarize_messages()
+# elif CLEAN_SUMMARY:
+#     clean_jsonl_data()
+#     summarize_messages()
 
-else:
-    bot_main()
-    nostr_main()
+# else:
+#     bot_main()
+
+nostr_main()
--- src/lib/nostr/nostr.py
+++ src/lib/nostr/nostr.py
@@ -1,42 +1,39 @@
 from lib.env import NOSTR_SEC
-import time
-import uuid
+from uuid import uuid1
 from binascii import unhexlify
-
-private_key = PrivateKey(unhexlify(NOSTR_SEC))
-public_key = private_key.public_key
-
 from pynostr.key import PrivateKey
 from pynostr.relay_manager import RelayManager
 from pynostr.filters import FiltersList, Filters
 from pynostr.event import EventKind
-'''
- 'wss://eden.nostr.land',
- 'wss://nostr.fmt.wiz.biz',
- 'wss://relay.damus.io',
- 'wss://nostr-pub.wellorder.net',
- 'wss://relay.nostr.info',
- 'wss://offchain.pub',
- 'wss://nos.lol',
- 'wss://brb.io',
- 'wss://relay.snort.social',
- 'wss://relay.current.fyi',
- 'wss://nostr.relayer.se',
-'''
+
+private_key = PrivateKey(unhexlify(NOSTR_SEC))
+public_key = private_key.public_key
+
 def nostr_main():
     relay_manager = RelayManager(timeout=2)
-    relay_manager.add_relay("wss://nostr-pub.wellorder.net")
-    relay_manager.add_relay("wss://relay.damus.io")
+    relay_manager.add_relay('wss://eden.nostr.land')
+    relay_manager.add_relay('wss://nostr.fmt.wiz.biz')
+    relay_manager.add_relay('wss://relay.damus.io')
+    relay_manager.add_relay('wss://nostr-pub.wellorder.net')
+    relay_manager.add_relay('wss://relay.nostr.info')
+    relay_manager.add_relay('wss://offchain.pub')
+    relay_manager.add_relay('wss://nos.lol')
+    relay_manager.add_relay('wss://brb.io')
+    relay_manager.add_relay('wss://relay.snort.social')
+    relay_manager.add_relay('wss://relay.current.fyi')
+    relay_manager.add_relay('wss://nostr.relayer.se')
     filters = FiltersList([Filters(kinds=[EventKind.TEXT_NOTE], limit=100)])
-    subscription_id = uuid.uuid1().hex
+    subscription_id = uuid1().hex
     relay_manager.add_subscription_on_all_relays(subscription_id, filters)
+    relay_manager.run_sync()
+
     while relay_manager.message_pool.has_notices():
         notice_msg = relay_manager.message_pool.get_notice()
-        print(notice_msg.content)
+        print('notice_msg.content', notice_msg.content)
     while relay_manager.message_pool.has_events():
         event_msg = relay_manager.message_pool.get_event()
-        print(event_msg.event.content)
-# relay_manager.close_all_relay_connections()
+        print('event_msg.event.content', event_msg.event.content)
+    relay_manager.close_all_relay_connections()
 
 '''
 Reply to a note
