05d2f7e037d822e8b5a54067fb0f11acb88e57f2
bnonni <bryan@nonni.io>
2023-08-11 07:55:36 -0400

    updates

--- sample.env.py
+++ sample.env.py
@@ -1,7 +1,7 @@
-TELEGRAM_BOT_TOKEN = ""
-OPENAI_API_KEY = ""
-STRIKE_API_KEY = ""
-NOSTR_NPUB = ""
-NOSTR_PUB = ""
-NOSTR_SEC = ""
-BOT_HANDLE = ""
+TELEGRAM_BOT_TOKEN = "" # Create a bot via the Bot Father: https://www.telegram.me/BotFather
+OPENAI_API_KEY = "" # Create an account with Open AI and generate an API key: https://platform.openai.com/
+STRIKE_API_KEY = "" # Must have a Strike for Business account: https://strike.me/developer/; feel free to use whatever payment service you like
+NOSTR_PUB = "" # Create any number of ways (snort, damus, noscli, etc.); hex pubkey
+NOSTR_SEC = "" # Create any number of ways (snort, damus, noscli, etc.); hex seckey
+NOSTR_NPUB = "" # Create any number of ways (snort, damus, noscli, etc.); semantic npub
+BOT_HANDLE = "" # Name of your bot
