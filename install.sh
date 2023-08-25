e41a8d26f97a48df1a535abe6536d9678886e665
bnonni <bryan@nonni.io>
2023-08-25 07:00:23 -0400

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
-source .venv/bin/activate
-pip install -r requirements.txt
-sudo cp abbot.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable abbot
-sudo systemctl start abbot
-sudo systemctl status abbot
-sudo systemctl start abbot
-sudo journalctl -u abbot | tail
-echo "Done!"
+
+setup_env () {
+    echo "Source .env"
+    # source .env/bin/activate
+    echo "Install requirements.txt"
+    # pip install -r requirements.txt
+    echo "Copy abbot.server to system"
+    # sudo cp abbot.service /etc/systemd/system
+}
+
+daemon_start () {
+    echo "Reload system daemon"
+    # sudo systemctl daemon-reload
+    echo "Enable abbot"
+    # sudo systemctl enable abbot
+    echo "Start abbot"
+    # sudo systemctl start abbot
+}
+
+daemon_status () {
+    echo "Check abbot status"
+    # sudo systemctl status abbot
+    echo "Print system logs"
+    # sudo journalctl -u abbot | tail
+}
+
+setup_env && \
+daemon_start && \
+daemon_status && \
+echo "Abbot is live!" && \
 exit 0
