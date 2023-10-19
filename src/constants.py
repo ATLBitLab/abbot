92cce60c60191d0a0e87bf69aeb69d5d88323e24
bnonni <bryan@nonni.io>
2023-10-19 00:13:08 -0400

    update abbit

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,5 @@
-from os.path import abspath
-
-I = 0
 OPENAI_MODEL = "gpt-4"
 THE_CREATOR = 1711738045
-OPTINOUT_FILEPATH = abspath("src/data/optin_optout.json")
 HELP_MENU = """""Welcome to Abbit!
 Available Commands
 1. /start
