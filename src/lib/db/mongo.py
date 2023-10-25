5b6347e4109875f1f74bd8044aedeec1c04f4c3f
bnonni <bryan@nonni.io>
2023-10-24 22:16:36 -0400

    adding pinecone

--- src/lib/db/mongo.py
+++ src/lib/db/mongo.py
@@ -2,3 +2,10 @@ from pymongo import MongoClient
 from lib.abbot.env import DATABASE_CONNECTION_STRING
 
 client = MongoClient(host=DATABASE_CONNECTION_STRING)
+
+nostr_db = client.get_database("nostr")
+nostr_notes = nostr_db.get_collection("notes")
+nostr_channels = nostr_db.get_collection("channels")
+
+telegram_db = client.get_database("telegram")
+telegram_channels = client.get_database("telegram")
