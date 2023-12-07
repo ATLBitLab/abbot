6a4488b74219b6c58c0cbaae2244ca5328538ad8
bnonni <bryan@nonni.io>
2023-12-06 19:34:39 -0500

    update main

--- src/main.py
+++ src/main.py
@@ -7,19 +7,19 @@ from lib.abbot.telegram_bot import TelegramBotBuilder
 
 if __name__ == "__main__":
     try:
-        if not DEV_MODE and not TEST_MODE:
-            raise AbbotException(
-                "Do not run in production mode unless you are sure: python src/main.py [--telegram | --nostr] [--dev | --test]"
-            )
-        elif TELEGRAM_MODE:
+        # if not DEV_MODE and not TEST_MODE:
+        #     raise AbbotException(
+        #         "Do not run in production mode unless you are sure: python src/main.py [--telegram | --nostr] [--dev | --test]"
+        #     )
+        if TELEGRAM_MODE:
             telegram_abbot: TelegramBotBuilder = TelegramBotBuilder()
             telegram_abbot.run()
         # elif NOSTR_MODE:
         #     nostr_abbot: NostrBotBuilder = NostrBotBuilder()
         #     nostr_abbot.add_relays_connect_and_start_client()
         #     nostr_abbot.run()
-        else:
-            raise AbbotException("Must specify platform: python src/main.py [--telegram | --nostr] [--dev | --test]")
+        # else:
+        #     raise AbbotException("Must specify platform: python src/main.py [--telegram | --nostr] [--dev | --test]")
     except KeyboardInterrupt:
         bot_debug.log("Interrupt received, shutting down.")
         bot_debug.log("Shutting down...")
